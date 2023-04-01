956f50ce5cf10e125e8ce2d0fe9f3a9a8521aea9
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2023-04-01 17:11:21 -0400

    mongodb changes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuit-controller.js";
 import cors from 'cors'
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter);
 
 
 const app = express()
